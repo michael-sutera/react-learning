e34acaa466332c356fa0afec8fba76fcca343e20
Sutera, Michael M <michael.sutera@revunit.com>
2018-08-20 13:01:10 -0500

    no message

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyDpTCFOYyAHh8kdnllaj5CIaZ3pnPl3qHM';
 
-// Create a new component. This component should produce some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+YTSearch({ key: API_KEY, term: 'surfboards' }, data => console.log(data));
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div >
+        );
+    }
 }
 
-// Take this component's generated HTML and put it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
